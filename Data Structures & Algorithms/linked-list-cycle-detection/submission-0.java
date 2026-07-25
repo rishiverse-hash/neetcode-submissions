8f7528325d90db5e0258b3f059d259028987e3c5
Adwaid Sajeevan <adwaidsajeevan8@gmail.com>
2026-07-25 12:03:39 +0530

    Update: linked-list-cycle-detection - submission-0

--- Data Structures & Algorithms/linked-list-cycle-detection/submission-0.java
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.java
@@ -10,17 +10,16 @@
  */
 
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1==null) return list2;   
-        if(list2==null) return list1;
-
-        if(list1.val<=list2.val){
-            list1.next=mergeTwoLists(list1.next,list2);
-            return list1;
-        } else {
-            list2.next = mergeTwoLists(list1, list2.next);
-            return list2;
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
         }
-        
+        return false;
     }
 }
